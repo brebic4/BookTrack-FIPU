2cd0bef5027bd925a9c1077142cd5774ddfec734
brebic4 <brebic@student.unipu.hr>
2026-06-14 12:05:30 +0200

    Spring Boot project initialization

---
+++ booktrack/src/test/java/hr/fipu/booktrack/BooktrackApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fipu.booktrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
