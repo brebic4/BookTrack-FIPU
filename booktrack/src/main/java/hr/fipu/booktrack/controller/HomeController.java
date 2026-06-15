4a0cfcaab4a2953084b1964d59308445591c19f5
brebic4 <brebic@student.unipu.hr>
2026-06-15 18:05:44 +0200

    Updated home page, implemented stats

--- booktrack/src/main/java/hr/fipu/booktrack/controller/HomeController.java
+++ booktrack/src/main/java/hr/fipu/booktrack/controller/HomeController.java
@@ -1,13 +1,28 @@
 package hr.fipu.booktrack.controller;
 
+import hr.fipu.booktrack.service.StatistikaService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private final StatistikaService statistikaService;
+
+    public HomeController(StatistikaService statistikaService) {
+        this.statistikaService = statistikaService;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("brojKnjiga", statistikaService.brojKnjiga());
+        model.addAttribute("brojDostupnih", statistikaService.brojDostupnihKnjiga());
+        model.addAttribute("brojPosudenih", statistikaService.brojPosudenihKnjiga());
+        model.addAttribute("brojKategorija", statistikaService.brojKategorija());
+        model.addAttribute("brojAktivnihPosudbi", statistikaService.brojAktivnihPosudbi());
+
         return "index";
     }
 }
